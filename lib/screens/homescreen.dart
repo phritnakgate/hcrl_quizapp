9fbb253d1874fd5145ff7114988466b974fc8679
phritnakgate <boss5500216@gmail.com>
2023-08-23 22:32:57 +0700

    feat: connect firebase firestore

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -14,31 +15,56 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-                title: const Text('Quiz App'),
-                leading: const Icon(Icons.menu),
-                actions: <Widget>[
-                  Padding(
-                      padding: const EdgeInsets.only(right: 20.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          FirebaseAuth.instance.signOut();
-                        },
-                        child: const Icon(
-                          Icons.logout_outlined,
-                          size: 26.0,
-                        ),
-                      ))
-                ]),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Sign in as ${FirebaseAuth.instance.currentUser}"),
-                ],
-              ),
-            )));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+              title: const Text('Quiz App'),
+              leading: const Icon(Icons.menu),
+              actions: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: const Icon(
+                      Icons.add,
+                      size: 26.0,
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        FirebaseAuth.instance.signOut();
+                      },
+                      child: const Icon(
+                        Icons.logout_outlined,
+                        size: 26.0,
+                      ),
+                    )),
+                
+              ]),
+          body: StreamBuilder(
+              stream: FirebaseFirestore.instance.collection('quiz').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.data == null) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(snapshot.data!.docs[index]['quiz_name']),
+                      subtitle: Text(snapshot.data!.docs[index]['quiz_desc']),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.arrow_forward_ios_rounded),
+                        onPressed: () {},
+                      ),
+                    );
+                  },
+                );
+              })),
+    );
   }
 }
