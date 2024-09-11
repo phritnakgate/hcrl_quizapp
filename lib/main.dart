f61d54cbb5cf4660bc579fdfe2ae7ef84615fa09
phritnakgate <boss5500216@gmail.com>
2024-09-11 22:33:40 +0700

    chore: change to compile-time constants for env

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,23 @@ import 'package:hcrl_quizapp/screens/welcomescreen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // if(kIsWeb){
-  //   await Firebase.initializeApp(
-  //     options: const FirebaseOptions(
-  //       apiKey: "",
-  //       appId: "",
-  //       messagingSenderId: "",
-  //       projectId: "",
-  //     ),
-  //   );
-  // }else{
-  await Firebase.initializeApp();
+  // Use compile-time constants for environment variables, injected via --dart-define
+  const String appId = String.fromEnvironment('APPID', defaultValue: '');
+  const String apiKey = String.fromEnvironment('APIKEY', defaultValue: '');
+  const String messagingSenderId = String.fromEnvironment('MESSAGESERVICEID', defaultValue: '');
+  const String projectId = String.fromEnvironment('PROJECTID', defaultValue: '');
+  const String databaseUrl = String.fromEnvironment('DATABASEURL', defaultValue: '');
+
+  // Initialize Firebase with environment variables
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      appId: appId,
+      apiKey: apiKey,
+      messagingSenderId: messagingSenderId,
+      projectId: projectId,
+      databaseURL: databaseUrl,
+    ),
+  );
   
   runApp(const StartScreen());
 }
